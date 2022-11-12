42e1939ca6ed632e1817da8e603af5b3a2485d4f
emjohnson3 <emjohnson3@madisoncollege.edu>
2022-11-11 22:57:21 -0600

    Attempting set up
    Copy + Paste ftw?

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
+
+interface QuizDisplay {
+  quizName: string;
+  quizQuestions: QuestionDisplay[];
+  markedForDelete: boolean;
+}
+
+interface QuestionDisplay {
+  questionText: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  
+  constructor(private quizSvc: QuizService) {};
+
+  ngOnInit() {
+    const data = this.quizSvc.loadQuizzes();
+    console.log(data);
+
+    this.quizzes = data.map(x => ({
+      quizName: x.name
+      , quizQuestions: x.questions.map((y: any) => ({
+        questionText: y.name
+      }))
+      , markedForDelete: false
+    }));
+    console.log(this.quizzes);
+  }
+
+  quizzes: QuizDisplay[] = [];
+
+  selectedQuiz: QuizDisplay | undefined = undefined;
+
+  selectQuiz = (quizToSelect: QuizDisplay) => {
+    this.selectedQuiz = quizToSelect;
+  };
+
+  addNewQuiz = () => {
+    
+    const newQuiz: QuizDisplay = {
+      quizName: 'Untitled Quiz'
+      , quizQuestions: []
+      , markedForDelete: false
+    };
+
+    this.quizzes = [
+      ...this.quizzes
+      , newQuiz
+    ];
+
+    this.selectQuiz(newQuiz);
+  }
+
+  addNewQuestion = () => {
+    if (this.selectedQuiz != undefined) {
+      this.selectedQuiz.quizQuestions = [
+        ...this.selectedQuiz.quizQuestions
+        , {
+          questionText: "New Question"
+        }
+      ]
+    }
+  };
+
+  removeQuestion = (questionToRemove: QuestionDisplay) => {
+    if (this.selectedQuiz != undefined) {
+      this.selectedQuiz.quizQuestions 
+        = this.selectedQuiz.quizQuestions
+          .filter(x => x !== questionToRemove)
+    }
+  };
+}
---
+++ src/app/quiz.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor() { }
+
+  loadQuizzes = (): any[] => {
+
+    const quizzesFromWeb = [
+      {
+        name: 'Quiz 1'
+        , questions: [
+          {
+            name: 'Question 1'
+          }
+          , {
+            name: 'Question 2'
+          }
+        ]
+      }
+      , {
+        name: 'Quiz 2'
+        , questions: []
+      }
+    ];
+
+    return quizzesFromWeb;
+  }
+}
